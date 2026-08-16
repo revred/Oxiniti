2e3cc5350c37fc9a7359e39eb16d3a55be2860c5
Divya Chekuri <divya@mak3r.ai>
2026-08-16 11:39:37 +0530

    Point top-bar WhatsApp icon to the Oxyniti Network group chat

--- SiteContact.cs
+++ SiteContact.cs
@@ -4,6 +4,7 @@ public static class SiteContact
 {
     public const string WhatsAppNumber = "919659727477";
     public const string PhoneDisplay = "+91 96597 27477";
+    public const string WhatsAppGroupLink = "https://chat.whatsapp.com/IlHfPXCNWk3LQa1LYPqceu?s=sw&p=i&mlu=4";
 
     public static string WhatsAppLink(string message = "Hi, I'd like to know more about Oxyniti.") =>
         $"https://wa.me/{WhatsAppNumber}?text={Uri.EscapeDataString(message)}";
